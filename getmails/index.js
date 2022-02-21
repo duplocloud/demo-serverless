3d14c0bc3630d0ddb39aab0556331e381e569521
Ganesh <ganesh@duplocloud.net>
2022-02-22 00:42:33 +0530

    test

--- getmails/index.js
+++ getmails/index.js
@@ -15,7 +15,7 @@
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': true,
     },
-    body: JSON.stringify(["ganesh@duploloud.net"]),
+    body: JSON.stringify(["ganesh@duploloud.net1"]),
   })
 }
 
